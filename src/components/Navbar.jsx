c3352b8390f9aaceb73a301f6ea1b418c902ab05
ansh10082007 <anshbomble37@gmail.com>
2026-07-30 17:55:04 +0000

    updated logout pop up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
       </div>
       <hr></hr>
       {showModal && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm">
+        <div className="fixed top-0 left-0 w-screen h-screen z-50  flex items-center justify-center bg-black/50 backdrop-blur-sm">
           <div className="w-full max-w-sm rounded-xl bg-white p-6 shadow-xl border border-gray-100">
             <h3 className="text-lg font-semibold text-gray-900">Confirm Logout</h3>
             <p className="mt-2 text-sm text-gray-500">Are you sure you want to log out of your account?</p>
