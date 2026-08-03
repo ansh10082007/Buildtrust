0bd24e3892fea498ed93f199b24c5f4aa4c86245
ansh10082007 <anshbomble37@gmail.com>
2026-08-03 17:43:06 +0000

    updated viewfull card

--- src/components/ViewFull_card.jsx
+++ src/components/ViewFull_card.jsx
@@ -7,11 +7,11 @@ const viewFull_card = (props) => {
     <div>
       <div className='flex items-baseline gap-52 h-30 w-full p-5 bg-blue-950'>
         <h1 className='flex items-baseline'><IndianRupee className='text-white' /><h1 className='font-bold text-3xl text-white'>{props.price}++</h1> <small className='text-sm font-medium text-gray-300'>@{props.price_sqft}/sqft</small></h1>
-        <h2> <div className='font-bold text-2xl text-gray-50 '>{props.bhk}</div> <smalll className="text-sm font-medium text-gray-400" >flats/apartments for sale in {props.name}</smalll> </h2>
+        <h2> <div className='font-bold text-2xl text-gray-50 '>{props.bhk} bhk</div> <smalll className="text-sm font-medium text-gray-400" >flats/apartments for sale in {props.name}</smalll> </h2>
       </div>
       <div className='flex min-h-screen bg-blue-50'>
         <div className='p-2'>
-          <ImagesCarousel image={props.img} img1={props.img1} img2={props.img2} img3={props.img3} />
+          <ImagesCarousel image={props.image} img1={props.img1} img2={props.img2} img3={props.img3} />
         </div>
         <div className='flex flex-col gap-10 ml-10 mt-10  bg-white/30 backdrop-blur-md shadow-md hover:shaow-lg rounded-3xl h-100 w-180 p-5 border-t-4 border-gray-700 mr-2 '>
           <div>
